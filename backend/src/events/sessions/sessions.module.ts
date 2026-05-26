735619fcb5d80acd0c96db0bec13714ffc7913e5
Niko Pozderec <nikopozderec36@gmail.com>
2026-05-26 12:10:27 +0200

    [fix] import UsersModule in SessionsModule so RolesGuard can resolve UsersRepository

--- backend/src/events/sessions/sessions.module.ts
+++ backend/src/events/sessions/sessions.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FirebaseModule } from '../../firebase/firebase.module';
+import { UsersModule } from '../../users/users.module';
 import { SessionsController } from './sessions.controller';
 import { SessionsService } from './sessions.service';
 import { SessionsRepository } from './sessions.repository';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [FirebaseModule, UsersModule],
   controllers: [SessionsController],
   providers: [SessionsService, SessionsRepository],
 })
